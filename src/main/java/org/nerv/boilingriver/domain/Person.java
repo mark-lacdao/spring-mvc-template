1c13dcda08b5b25796a4ad3398322a7f65ba9c38
Mark Jaison Lacdao <mark.lacdao@ft.com>
2014-05-29 15:54:56 +0800

    Added pre hibernate practice files. Added log4j config.

---
+++ src/main/java/org/nerv/boilingriver/domain/Person.java
@@ -0,0 +1,11 @@
+package org.nerv.boilingriver.domain;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Created by mark.lacdao on 29/05/2014.
+ */
+@Entity
+public class Person implements Serializable {
+}
